070ec1a9c1fda56297f48073d894345887583708
Manar-Alzoubi <mzzo3bi20988@gmail.com>
2022-03-28 00:34:13 +0300

    lab14

---
+++ src/test/java/com/example/jBCryptAuth/JBCryptAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jBCryptAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBCryptAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
